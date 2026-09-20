04e9804c6198e654598ab909653e618ae44f09c5
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-20 16:56:20 -0700

    Slowly working on it

--- PointersTest.cpp
+++ PointersTest.cpp
@@ -1,1 +1,19 @@
 //COMSC-210 | Lab 11 | Eric-Giulio Hedes
+#include <iostream>
+using namespace std;
+
+const int MAX_VAL = 3;
+
+struct Flower
+{
+    string name, color;
+    int petals;
+};
+
+void setUpFlowers();
+void printFlowers();
+
+int main()
+{
+
+}
