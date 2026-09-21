b7472f966069d16389219e390b13552dcb2b7e09
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-20 17:34:52 -0700

    Starting to add comments; 50% done

--- PointersTest.cpp
+++ PointersTest.cpp
@@ -1,34 +1,47 @@
 //COMSC-210 | Lab 11 | Eric-Giulio Hedes
+/*NOTE: The real-life situation I'm gonna use here is about flowers and gardening; while it may
+be quite simpler than expected, it'll still be asking the user for what flowers they would like
+to pick for their small garden. Have fun!*/
 #include <iostream>
 using namespace std;
 
+//Declare the constant integer named MAX_VAL; will be useful for limits
 const int MAX_VAL = 3;
 
+//Define the Flower struct
 struct Flower
 {
     string name, color;
     int petals;
 };
 
+//Function prototypes
 void setUpFlowers(Flower *ptr, int num);
 void printFlowers(Flower *ptr, int num);
 
+//Start of main()
 int main()
 {
+    //Declare the garden struct
     Flower *garden = new Flower[MAX_VAL];
 
+    //Use a string for the 'flower' word; will be plural if the MAX_VAL equals to one.
     string fl_string;
     if (MAX_VAL == 1) fl_string = "flower";
     else fl_string = "flowers";
 
+    //Print a welcoming message
     cout << "Welcome to the flower algorithm! Please define the " << MAX_VAL << " " << fl_string << " to set up your garden." << endl << endl;
     for (int i = 0; i < MAX_VAL; i++) setUpFlowers(&garden[i], i);
     cout << "Well done setting up your garden! Your list is as follows:" << endl << endl;
     for (int i = 0; i < MAX_VAL; i++) printFlowers(&garden[i], i);
 }
+//End of main()
 
+//Define setUpFlowers(); will be used for initializing flower structs
 void setUpFlowers(Flower *ptr, int num)
 {
+    //
     cout << "Flower #" << num + 1 << ":" << endl << "------------------------" << endl;
     cout << "Name: ";
     getline(cin, ptr->name);
@@ -40,6 +53,7 @@ void setUpFlowers(Flower *ptr, int num)
     cout << endl;
 }
 
+//Define printFlowers(); will be used for printing all flowers' elements.
 void printFlowers(Flower *ptr, int num)
 {
     cout << "Flower #" << num << " summary:" << endl << "-----------------------" << endl;
