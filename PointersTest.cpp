a081d92d63128d6636d7578cc0011a5b492c6f7c
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-20 17:22:55 -0700

    Finished up printFlowers()

--- PointersTest.cpp
+++ PointersTest.cpp
@@ -23,7 +23,8 @@ int main()
 
     cout << "Welcome to the flower algorithm! Please define the " << MAX_VAL << " " << fl_string << " to set up your garden." << endl << endl;
     for (int i = 0; i < MAX_VAL; i++) setUpFlowers(&garden[i], i);
-    //for (int i = 0; i < MAX_VAL; i++) printFlowers(&garden[i], i);
+    cout << "Well done setting up your garden! Your list is as follows:" << endl << endl;
+    for (int i = 0; i < MAX_VAL; i++) printFlowers(&garden[i], i);
 }
 
 void setUpFlowers(Flower *ptr, int num)
@@ -33,7 +34,17 @@ void setUpFlowers(Flower *ptr, int num)
     getline(cin, ptr->name);
     cout << "Color: ";
     getline(cin, ptr->color);
-    cout << "Number of petals:";
+    cout << "Number of petals: ";
     cin >> ptr->petals;
     cin.ignore(1000, 10);
+    cout << endl;
+}
+
+void printFlowers(Flower *ptr, int num)
+{
+    cout << "Flower #" << num << " summary:" << endl << "-----------------------" << endl;
+    cout << "Name: " << ptr->name << endl;
+    cout << "Color: " << ptr->color << endl;
+    cout << "Number of petals: " << ptr->petals << endl;
+    cout << endl;
 }
